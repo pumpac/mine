41f76ea36e5c2688ee8f97c2c8fa8f349ab4451b
pumpac <z7gagaya@outlook.com>
2024-08-27 13:32:22 +0800

    Update mine.js

--- mine.js
+++ mine.js
@@ -2,7 +2,7 @@ const config = require('./config.js');
 const ethers = require('ethers');
 const provider = new ethers.JsonRpcProvider(config.RPC);
 const miner = new ethers.Wallet(config.PRIVATE_KEY, provider);
-const HYPERSOUND_ADDRESS = '0x8233eD6E03C6085c36FB83e20De1d35763426F11';
+const HYPERSOUND_ADDRESS = '0x284c8e910098B737bfEada09A5fC27dfA8020FC0';
 const HYEPRSOUND_ABI = require('./abi.json')
 const hypersound = new ethers.Contract(HYPERSOUND_ADDRESS, HYEPRSOUND_ABI, miner);
 
